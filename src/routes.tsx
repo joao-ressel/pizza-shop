1b2ac0717f06cd6072c8a0afd7b31f10b10f7f94
João Carlos Ressel <joao.resselti@gmail.com>
2024-12-03 13:51:27 -0500

    feito a pagina de login, cadastro e pedidos usando o react-hookform, o tosat do sonner e o feito o theme toggle para alteração de tema.

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,21 +3,27 @@ import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
+import { SignUp } from "./pages/auth/sign-up";
+import { Orders } from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
+    ],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      { path: "/sign-in", element: <SignIn /> }, 
+      { path: "/sign-up", element: <SignUp /> }, 
+    ],
+  },
 
-    {
-        path:'/',
-        element: <AppLayout/>,
-        children: [{path:'/', element: <Dashboard/>}],
-    },
-    {
-        path:'/sign-in',
-        element: <AuthLayout/>,
-        children: [{path:'/', element: <SignIn/>}],
-    }
-
-
-    // {path:'/', element: <Dashboard/>},
-    // {path:'/sign-in', element: <SignIn/>}
-])
+  // {path:'/', element: <Dashboard/>},
+  // {path:'/sign-in', element: <SignIn/>}
+]);
