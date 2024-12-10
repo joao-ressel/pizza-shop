017b2862e64443f5a7d99da109f29bfa4b2cf8a5
João Carlos Ressel <joao.resselti@gmail.com>
2024-12-10 11:38:45 -0500

    página de pedidos atualizada com componentes e paginação do radix-ui, criando componentes de detalhamento e separando tabela e paginação.

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,48 @@
+import { TableCell, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Search, X } from "lucide-react";
+import { Dialog } from "@/components/ui/dialog";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { OrderDetails } from "./order-details";
+
+export function OrderTableRow() {
+  return (
+    <>
+      <TableRow>
+        <TableCell>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline" size="xs">
+                <Search className="h-3 w-3" />
+                <span className="sr-only">Detalhes do pedido</span>
+              </Button>
+            </DialogTrigger>
+            <OrderDetails/>
+          </Dialog>
+        </TableCell>
+        <TableCell className="font-mono font-medium">a4s97sda123s6d</TableCell>
+        <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+        <TableCell>
+          <div className="flex items-center gap-2">
+            <span className="h-2 w-2 rounded-full bg-slate-400" />
+            <span className="font-medium text-muted-foreground">Pendente</span>
+          </div>
+        </TableCell>
+        <TableCell className="font-medium">Diego Fernandes</TableCell>
+        <TableCell className="font-medium">R$ 149,90</TableCell>
+        <TableCell>
+          <Button variant="ghost" size="xs">
+            <ArrowRight className="mr-2 h-3 w-3" />
+            Aprovar
+          </Button>
+        </TableCell>
+        <TableCell>
+          <Button variant="ghost" size="xs">
+            <X className="mr-2 h-3 w-3" />
+            Cancelar
+          </Button>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+}
