fc92bb3a3a50ce774b1d4466bb1cc5bfb3f21144
joao-ressel <joao.resselti@gmail.com>
2025-02-07 11:30:59 -0500

    atualizando projeto com o setup de testes e iniciando o teste unitario do componente order status

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+/// <reference types="vitest" />
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './src/test/setup.ts',
+  },
+});
